68858e619ebca1ab0b22d500af4b9dcbe201d85e
Yarlei Cruz <yarleicruz1998@gmail.com>
2023-11-06 18:29:01 -0300

    Resolvendo hardcoded-assertions

--- cypress/e2e/hardcoded-assertion/sample.cy.js
+++ cypress/e2e/hardcoded-assertion/sample.cy.js
@@ -1,5 +1,8 @@
+const {hits} = require('../../fixtures/stories')
+
 describe('Hardcoded assertion bad practice', () => {
   beforeEach(() => {
+
     cy.intercept(
       'GET',
       '**/search**',
@@ -16,12 +19,12 @@ describe('Hardcoded assertion bad practice', () => {
 
     cy.get('.table-row')
       .as('tableRows')
-      .should('have.length', 2)
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing')
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code')
+      .should('have.length', hits.length)
+
+    hits.forEach((hit, index) => {
+      cy.get('@tableRows')
+        .eq(index)
+        .should('contain', hit.title)
+    });
   })
 })
