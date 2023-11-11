80a0360a13efc4baaffbcb2131b396ed615f8aae
Yarlei Cruz <yarleicruz1998@gmail.com>
2023-11-11 14:00:42 -0300

    slow tests

--- cypress/e2e/slow-tests/sample1.cy.js
+++ cypress/e2e/slow-tests/sample1.cy.js
@@ -1,12 +1,15 @@
+const {hits} = require('../../fixtures/stories')
+
 describe('Slow tests bad practice - use the API to test the frontend', () => {
   beforeEach(() => {
     cy.intercept(
       'GET',
-      '**/search**'
+      '**/search**',
+      {fixture:'stories'}
+
     ).as('getStories')
 
     cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
 
     cy.get('input[type="text"]')
       .should('be.visible')
@@ -19,9 +22,7 @@ describe('Slow tests bad practice - use the API to test the frontend', () => {
     cy.get('@searchField')
       .type('frontend testing{enter}')
 
-    cy.wait('@getStories')
-
     cy.get('.table-row')
-      .should('have.length', 100)
+      .should('have.length', hits.length)
   })
 })
